8478b36a139f83482a66e0462eca6ad822f148a0
priyanshjain06 <priyansh01jain@gmail.com>
2024-08-21 14:29:08 +0530

    added

--- 11_Redux_Toolkit/src/components/AddTodo.jsx
+++ 11_Redux_Toolkit/src/components/AddTodo.jsx
@@ -1,7 +1,7 @@
 import React, {useState} from 'react'
 import {useDispatch} from 'react-redux'
-import {addTodo} from '../features/todo/todoSlice' 
-import 
+import {addTodo} from '../features/todo/todoSlice'
+
 function AddTodo() {
 
     const [input, setInput] = useState('')
