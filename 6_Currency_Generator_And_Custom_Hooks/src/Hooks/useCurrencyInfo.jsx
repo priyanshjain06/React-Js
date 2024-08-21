1ecebe5953d4535ec6641fd27937d719bfcafe3a
priyanshjain06 <priyansh01jain@gmail.com>
2024-08-21 18:30:12 +0530

    added

--- 6_Currency_Generator_And_Custom_Hooks/src/Hooks/useCurrencyInfo.jsx
+++ 6_Currency_Generator_And_Custom_Hooks/src/Hooks/useCurrencyInfo.jsx
@@ -10,7 +10,7 @@ function useCurrencyInfo(currency) {
       .then((res) => setData(res[currency]));
     console.log(data);
   }, [currency]);
-  console.log(data);
+  console.log(data);added
   return data;
 }
 
