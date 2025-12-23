3d0f1f24e807d52c7ff4897710c12ac4d12b26fd
priyanshjain06 <priyansh01jain@gmail.com>
2025-12-23 18:57:02 +0530

    updatd !!

--- 11_Redux_Toolkit/src/features/todo/todoSlice.js
+++ 11_Redux_Toolkit/src/features/todo/todoSlice.js
@@ -1,7 +1,7 @@
-import {createSlice, nanoid } from '@reduxjs/toolkit';
+import { createSlice, nanoid } from '@reduxjs/toolkit';
 
 const initialState = {
-    todos: [{id: 1, text: "Hello world"}]
+    todos: [{ id: 1, text: "Hello world" }]
 }
 
 
@@ -11,24 +11,24 @@ export const todoSlice = createSlice({
     reducers: {
         addTodo: (state, action) => {
             const todo = {
-                id: nanoid(), 
+                id: nanoid(),  //REVIEW - or use UseID Hook
                 text: action.payload // it will bring  info about the event being occured
             }
             state.todos.push(todo)
         },
         removeTodo: (state, action) => {
-            state.todos = state.todos.filter((todo) => todo.id !== action.payload )
+            state.todos = state.todos.filter((todo) => todo.id !== action.payload)
         },
     }
 })
 
-export const {addTodo, removeTodo} = todoSlice.actions
+export const { addTodo, removeTodo } = todoSlice.actions
 
 export default todoSlice.reducer
 
+//REVIEW -  
 // action.payload contains the text for the new to-do item. For example, if you dispatch addTodo("Buy milk"), the payload would be "Buy milk", and the new to-do item will have "Buy milk" as its text.
 
 // state.todos = state.todos.filter((todo) => todo.id !== action.payload )
-
 
 // Here, action.payload contains the id of the to-do item that should be removed. For example, if you dispatch removeTodo(1), the payload would be 1, and the to-do item with id equal to 1 would be removed from the todos array.
